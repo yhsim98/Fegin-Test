4689b1b7bd0769beaa150622048f0f25bb621b32
심용현(팀) <yonghyeon.sim@pluglink.kr>
2022-11-09 21:29:32 +0900

    init

--- src/main/java/com/example/eksa/lib/BClient.java
+++ src/main/java/com/example/eksa/lib/BClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.Map;
 
 @Component
-@FeignClient(value="example", url = "k8s-hello-testingr-db50395e52-753162095.ap-northeast-2.elb.amazonaws.com")
+@FeignClient(value="example", url = "k8s-hello-testingr-db50395e52-753162095.ap-northeast-2.elb.amazonaws.com/B")
 public interface BClient {
     @GetMapping("/")
     Map<String, String> getB();
